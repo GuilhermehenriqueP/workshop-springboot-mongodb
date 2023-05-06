dce8b2e97f14a8bf05a3ffd03bce8aeda8d5f8d2
Guilherme Pessoa <gui.senai@live.com>
2023-05-06 09:57:31 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/pessoaguilherme/workshopmongo/resources/UserResource.java
+++ src/main/java/com/pessoaguilherme/workshopmongo/resources/UserResource.java
@@ -1,28 +1,26 @@
 package com.pessoaguilherme.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pessoaguilherme.workshopmongo.domain.User;
+import com.pessoaguilherme.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
+	
+	@Autowired
+	private UserService service;
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "Maria Silva", "maria.silva@gmail.com");
-		User alex = new User("2", "Alex Green", "alex.gren@gmail.com");
 
-		List<User> list = new ArrayList<>();
-
-		list.addAll(Arrays.asList(maria, alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
