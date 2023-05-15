13fa643d0f780c250226ddcf94030ce1acf98b07
Guilherme Pessoa <gui.senai@live.com>
2023-05-15 19:54:47 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/pessoaguilherme/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.pessoaguilherme.workshopmongo.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pessoaguilherme.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+
+}
