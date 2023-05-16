67285d35b9b4dc966fa19caacf6960abd6506914
Guilherme Pessoa <gui.senai@live.com>
2023-05-15 22:02:58 -0300

    Obtendo um post por id

---
+++ src/main/java/com/pessoaguilherme/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.pessoaguilherme.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pessoaguilherme.workshopmongo.domain.Post;
+
+import com.pessoaguilherme.workshopmongo.repository.PostRepository;
+import com.pessoaguilherme.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+		
+	}
+	
+}
