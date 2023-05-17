ba552ccc99e4d1175b1780faace7debc19cd7fc5
Guilherme Pessoa <gui.senai@live.com>
2023-05-16 22:09:19 -0300

    Consulta simples com @Query

--- src/main/java/com/pessoaguilherme/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pessoaguilherme/workshopmongo/repository/PostRepository.java
@@ -4,6 +4,7 @@ package com.pessoaguilherme.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pessoaguilherme.workshopmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.pessoaguilherme.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title' : { $regex: ?0 , $options: 'i' } }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
